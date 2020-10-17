f1b691027584997ab10fc3a1e31612777f5aaf82
Dan6erbond <moravrav@gmail.com>
2020-10-17 17:24:44 +0200

    Move general app layout to `Layout.vue`

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
+  <v-container>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img src="/res/img/Jenyus.png" class="my-3" contain height="60%" />
+      </v-col>
+    </v-row>
   </div>
+  </v-container>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
