e6e1dfec402b0ef2c85cec102aad087efaa0282c
Dan6erbond <moravrav@gmail.com>
2020-10-17 18:33:12 +0200

    Update formatting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container>
-  <div class="home">
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img src="/res/img/Jenyus.png" class="my-3" contain height="60%" />
-      </v-col>
-    </v-row>
-  </div>
+    <div class="home">
+      <v-row class="text-center">
+        <v-col cols="12">
+          <v-img src="/res/img/Jenyus.png" class="my-3" contain height="60%" />
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
