bb5ab75509f0371ea5c05d735001ba6cd1c7a6c1
Dan6erbond <moravrav@gmail.com>
2020-10-17 17:33:58 +0200

    Setup Vuex state to persist selected locale and make route include selected lang

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import i18n from './plugins/i18n';
 import vuetify from './plugins/vuetify';
-import router from './router'
-import i18n from './i18n'
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
   i18n,
-  render: h => h(App)
-}).$mount('#app')
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
