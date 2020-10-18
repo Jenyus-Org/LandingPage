fdf77dba097d078df85d9e693994f1d6de8d8f47
Dan6erbond <moravrav@gmail.com>
2020-10-18 08:58:27 +0200

    Add Vue appear transitions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="home">
-    <v-card flat tile height="100%" dark class="text-center py-4">
-      <img src="/res/img/Jenyus.png" contain height="60%" class="jenyus-logo" />
-      <div class="mb-4">
-        <v-card-title class="d-inline-block my-4">
-          {{ $t("tagline") }}
-        </v-card-title>
-        <br />
-        <v-btn color="#CC0033" dark rounded :to="{ name: 'About' }">{{
-          $t("aboutUs")
-        }}</v-btn>
-      </div>
-    </v-card>
+    <transition appear name="fade">
+      <v-card flat tile height="100%" dark class="text-center py-4">
+        <img
+          src="/res/img/Jenyus.png"
+          contain
+          height="60%"
+          class="jenyus-logo"
+        />
+        <div class="mb-4">
+          <v-card-title class="d-inline-block my-4">
+            {{ $t("tagline") }}
+          </v-card-title>
+          <br />
+          <v-btn color="#CC0033" dark rounded :to="{ name: 'About' }">{{
+            $t("aboutUs")
+          }}</v-btn>
+        </div>
+      </v-card>
+    </transition>
   </div>
 </template>
 
@@ -27,6 +34,14 @@
 
 .v-card__title {
   word-break: normal;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 
 @media (max-aspect-ratio: 9/16) {
