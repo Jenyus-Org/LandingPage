571412c32103eaf918b4a8d168c1324d5f97c117
Dan6erbond <moravrav@gmail.com>
2021-02-06 21:55:10 +0100

    Add Google webfonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,8 @@
 
 // You can delete this file if you're not using it
 import "./src/styles/global.css";
+import "fontsource-russo-one";
+import "fontsource-maven-pro";
 
 if (process.env.NODE_ENV === "development") {
   require("./src/styles/tailwind.css");
