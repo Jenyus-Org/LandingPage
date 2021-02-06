f20672afc6dc0894f512f192bdf14e2d3e5d1c2a
Dan6erbond <moravrav@gmail.com>
2021-02-06 23:17:43 +0100

    Add about and team members sections

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,69 @@
 import * as React from "react";
 import clsx from "clsx";
+import { graphql, Link, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 
 // markup
 const IndexPage = () => {
+  const { laptopImg, dominikImg, raviImg } = useStaticQuery(
+    graphql`
+      query IndexPageQuery {
+        laptopImg: file(relativePath: { eq: "laptop-splash.jpg" }) {
+          childImageSharp {
+            fluid(maxHeight: 2560) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        dominikImg: file(relativePath: { eq: "dominik.jpg" }) {
+          childImageSharp {
+            fluid(maxHeight: 450) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        raviImg: file(relativePath: { eq: "ravi_2_1_cropped_1.png" }) {
+          childImageSharp {
+            fluid(maxHeight: 450) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `,
+  );
+
   return (
     <main>
       <title>Home</title>
-      <div
-        className={clsx("flex", "items-center", "h-screen", "justify-between")}>
-        <div className={clsx("flex-1")}></div>
-        <div className={clsx("flex-1", "flex", "flex-col", "items-center")}>
+      <section
+        className={clsx(
+          "flex",
+          "items-stretch",
+          "h-screen",
+          "justify-between",
+        )}>
+        <div
+          className={clsx(
+            "flex-1",
+            "flex",
+            "justify-center",
+            "items-center",
+            "overflow-hidden",
+          )}>
+          <Img
+            className={clsx("min-w-full", "min-h-full", "flex-shrink-0")}
+            fluid={laptopImg?.childImageSharp.fluid}
+          />
+        </div>
+        <div
+          className={clsx(
+            "flex-1",
+            "flex",
+            "flex-col",
+            "items-center",
+            "justify-center",
+          )}>
           <div>
             <h1 className={clsx("font-title", "text-6xl", "mb-1")}>
               Jenyus Org
@@ -18,7 +72,149 @@ const IndexPage = () => {
             <p className={clsx("font-title", "text-xl")}>Realising Ideas</p>
           </div>
         </div>
-      </div>
+      </section>
+      <section
+        className={clsx("px-8", "pt-10", "max-w-screen-lg", "mx-auto", "mb-8")}>
+        <h2 className={clsx("font-title", "text-4xl", "mb-2")}>
+          About Jenyus Org
+        </h2>
+        <p className={clsx("mb-4")}>
+          Jenyus Org is a software development firm based in Switzerland.
+          <br />
+          <br />
+          Founded by two graduates of the engineering college in Aarau, they aim
+          to revolutionize processes in the education sector and provide
+          businesses with an involved process in creating modern software.
+          <br />
+          <br />
+          Need a website for your business, an internal management tool or have
+          any other ideas?
+        </p>
+        <div className={clsx("text-center", "block")}>
+          <Link
+            className={clsx(
+              "px-12",
+              "py-6",
+              "rounded-full",
+              "inline-block",
+              "bg-gradient-to-br",
+              "from-blue-700",
+              "to-red-400",
+              "transition-shadow",
+              "hover:shadow-xl",
+              "ring-4",
+              "ring-transparent",
+              "hover:ring-gray-200",
+              "text-white",
+              "font-bold",
+            )}
+            to="/contact">
+            Contact Us
+          </Link>
+        </div>
+      </section>
+      <section
+        className={clsx("px-8", "pt-4", "pb-8", "max-w-screen-lg", "mx-auto")}>
+        <h2 className={clsx("font-title", "text-4xl", "mb-4")}>
+          Meet The Team
+        </h2>
+        <div className={clsx("grid", "grid-cols-1", "md:grid-cols-2", "gap-4")}>
+          <div
+            className={clsx(
+              "flex",
+              "flex-col",
+              "rounded",
+              "shadow",
+              "py-4",
+              "px-8",
+              "items-center",
+              "justify-between",
+              "bg-blue-50",
+            )}>
+            <div
+              className={clsx(
+                "inline-block",
+                "w-36",
+                "h-36",
+                "rounded-full",
+                "overflow-hidden",
+                "mb-4",
+                "ring-4",
+                "ring-blue-100",
+              )}>
+              <Img
+                className={clsx("object-cover")}
+                fluid={dominikImg?.childImageSharp.fluid}
+              />
+            </div>
+            <div className={clsx("text-center", "mb-6")}>
+              <span className={clsx("font-extrabold")}>Dominik</span>
+              <br />
+              <span>Berger</span>
+            </div>
+            <div className={clsx("w-full", "mb-4")}>
+              <span className={clsx("text-2xl")}>Developer</span>
+              <br />
+              <span>CEO</span>
+            </div>
+            <div className={clsx("my-auto")}></div>
+            <div>
+              <p>
+                Dominik is has written a dissertation on online marketing and
+                SEO. He is an avid React and C# developer and works on pretty
+                much any software idea that comes to his mind.
+              </p>
+            </div>
+          </div>
+          <div
+            className={clsx(
+              "flex",
+              "flex-col",
+              "rounded",
+              "shadow",
+              "py-4",
+              "px-8",
+              "items-center",
+              "justify-between",
+              "bg-red-50",
+            )}>
+            <div
+              className={clsx(
+                "inline-block",
+                "w-36",
+                "h-36",
+                "rounded-full",
+                "overflow-hidden",
+                "mb-4",
+                "ring-4",
+                "ring-red-100",
+              )}>
+              <Img
+                className={clsx("object-cover")}
+                fluid={raviImg?.childImageSharp.fluid}
+              />
+            </div>
+            <div className={clsx("text-center", "mb-6")}>
+              <span className={clsx("font-extrabold")}>RaviAnand</span>
+              <br />
+              <span>Mohabir</span>
+            </div>
+            <div className={clsx("w-full")}>
+              <span className={clsx("text-2xl")}>Developer</span>
+              <br />
+              <span>CTO</span>
+            </div>
+            <div className={clsx("my-auto")}></div>
+            <div>
+              <p>
+                RaviAnand is an experienced developer, familiar with various
+                technologies and can help bring your next software project to
+                life in a professional manner.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,9 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      ringColor: ["hover", "active"],
+    },
   },
   plugins: [],
 };
