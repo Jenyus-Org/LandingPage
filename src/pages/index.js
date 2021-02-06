cb1e3e1bb999e8475b47cc7d1598bb74f23ff9b6
Dan6erbond <moravrav@gmail.com>
2021-02-06 22:07:23 +0100

    Add `clsx` helper library

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import * as React from "react";
+import clsx from "clsx";
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <title>Home</title>
-      <div className="flex items-center h-screen justify-between">
-        <div className="flex-1"></div>
-        <div className="flex-1 flex flex-col items-center">
+      <div
+        className={clsx("flex", "items-center", "h-screen", "justify-between")}>
+        <div className={clsx("flex-1")}></div>
+        <div className={clsx("flex-1", "flex", "flex-col", "items-center")}>
           <div>
-            <h1 className="font-title text-6xl mb-1">Jenyus Org</h1>
-            <hr className="border-2 border-black mb-2" />
-            <p className="font-title text-xl">Realising Ideas</p>
+            <h1 className={clsx("font-title", "text-6xl", "mb-1")}>
+              Jenyus Org
+            </h1>
+            <hr className={clsx("border-2", "border-black", "mb-2")} />
+            <p className={clsx("font-title", "text-xl")}>Realising Ideas</p>
           </div>
         </div>
       </div>
