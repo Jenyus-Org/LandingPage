93630c3f77fc3ff7c104008a271ca4862302994b
Dan6erbond <moravrav@gmail.com>
2021-02-07 21:44:08 +0100

    Add animations to project articles

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,9 +1,11 @@
 import clsx from "clsx";
+import { motion } from "framer-motion";
 import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
 import * as React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import ViewableMonitor from "../components/viewable-monitor";
 
 const ProjectsPage = () => {
   const { spesenAppScreenshot, recogLogo } = useStaticQuery(
@@ -49,102 +51,220 @@ const ProjectsPage = () => {
           </p>
         </div>
       </section>
-      <article
-        className={clsx(
-          "flex",
-          "flex-wrap",
-          "items-stretch",
-          "h-screen",
-          "justify-between",
-          "flex-col",
-          "md:flex-row",
-        )}>
-        <div
-          className={clsx(
-            "flex-1",
-            "flex",
-            "justify-center",
-            "items-center",
-            "bg-gradient-to-br",
-            "from-blue-700",
-            "to-red-400",
-            "h-full",
-          )}>
-          <Img
-            className={clsx("h-full", "md:h-3/4", "w-full", "m-6")}
-            imgStyle={{ objectFit: "contain" }}
-            fluid={spesenAppScreenshot?.childImageSharp.fluid}
-          />
-        </div>
-        <div
-          className={clsx(
-            "flex-1",
-            "flex",
-            "flex-col",
-            "items-center",
-            "justify-center",
-          )}>
-          <div className={clsx("p-8", "md:p-16")}>
-            <h2 className={clsx("text-2xl")}>Spesen App</h2>
-            <br />
-            <p>
-              Spesen App is a web app custom-built for the alte Kanti Aarau. By
-              leveraging the capabilities of modern single-page apps, and NodeJS
-              based webservers, teachers can make requests for vacation and
-              compensation through a mobile accessible website and view their
-              grants as well as other history directly using their active
-              directory logins.
-            </p>
-          </div>
-        </div>
-      </article>
-      <article
-        className={clsx(
-          "flex",
-          "flex-wrap",
-          "items-stretch",
-          "h-screen",
-          "justify-between",
-          "flex-col",
-          "md:flex-row",
-        )}>
-        <div
-          className={clsx(
-            "flex-1",
-            "flex",
-            "flex-col",
-            "items-center",
-            "justify-center",
-          )}>
-          <div className={clsx("p-8", "md:p-16")}>
-            <h2 className={clsx("text-2xl")}>Recog</h2>
-            <br />
-            <p>
-              Recog is a forum built in collaboration with the alte Kanti Aarau.
-              It aims to connect students and business acquaintances across
-              Switzerland with another and create a platform on which
-              collaboration and a modern take on education is effortless.
-            </p>
-          </div>
-        </div>
-        <div
-          className={clsx(
-            "flex-1",
-            "flex",
-            "justify-center",
-            "items-center",
-            "bg-gradient-to-br",
-            "from-blue-700",
-            "to-red-400",
-            "h-full",
-          )}>
-          <Img
-            className={clsx("h-full", "w-3/4", "md:w-1/2", "m-6")}
-            imgStyle={{ objectFit: "contain" }}
-            fluid={recogLogo?.childImageSharp.fluid}
-          />
-        </div>
-      </article>
+      <ViewableMonitor threshold={0.25}>
+        {(visible) => (
+          <motion.article
+            className={clsx(
+              "flex",
+              "flex-wrap",
+              "items-stretch",
+              "h-screen",
+              "justify-between",
+              "flex-col",
+              "md:flex-row",
+            )}
+            initial={false}
+            animate={visible ? "visible" : "hidden"}>
+            <motion.div
+              className={clsx(
+                "flex-1",
+                "flex",
+                "justify-center",
+                "items-center",
+                "bg-gradient-to-br",
+                "from-blue-700",
+                "to-red-400",
+                "h-full",
+              )}
+              variants={{
+                visible: {
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    y: { stiffness: 1000, velocity: -100 },
+                  },
+                },
+                hidden: {
+                  y: 50,
+                  opacity: 0,
+                  transition: {
+                    y: { stiffness: 1000 },
+                  },
+                },
+              }}>
+              <Img
+                className={clsx("h-full", "md:h-3/4", "w-full", "m-6")}
+                imgStyle={{ objectFit: "contain" }}
+                fluid={spesenAppScreenshot?.childImageSharp.fluid}
+              />
+            </motion.div>
+            <div
+              className={clsx(
+                "flex-1",
+                "flex",
+                "flex-col",
+                "items-center",
+                "justify-center",
+              )}>
+              <motion.div
+                className={clsx("p-8", "md:p-16")}
+                transition={{ staggerChildren: 0.07 }}>
+                <motion.h2
+                  className={clsx("text-2xl")}
+                  variants={{
+                    visible: {
+                      y: 0,
+                      opacity: 1,
+                      transition: {
+                        y: { stiffness: 1000, velocity: -100 },
+                      },
+                    },
+                    hidden: {
+                      y: 50,
+                      opacity: 0,
+                      transition: {
+                        y: { stiffness: 1000 },
+                      },
+                    },
+                  }}>
+                  Spesen App
+                </motion.h2>
+                <br />
+                <motion.p
+                  variants={{
+                    visible: {
+                      y: 0,
+                      opacity: 1,
+                      transition: {
+                        y: { stiffness: 1000, velocity: -100 },
+                      },
+                    },
+                    hidden: {
+                      y: 50,
+                      opacity: 0,
+                      transition: {
+                        y: { stiffness: 1000 },
+                      },
+                    },
+                  }}>
+                  Spesen App is a web app custom-built for the alte Kanti Aarau.
+                  By leveraging the capabilities of modern single-page apps, and
+                  NodeJS based webservers, teachers can make requests for
+                  vacation and compensation through a mobile accessible website
+                  and view their grants as well as other history directly using
+                  their active directory logins.
+                </motion.p>
+              </motion.div>
+            </div>
+          </motion.article>
+        )}
+      </ViewableMonitor>
+      <ViewableMonitor threshold={0.25}>
+        {(visible) => (
+          <motion.article
+            className={clsx(
+              "flex",
+              "flex-wrap",
+              "items-stretch",
+              "h-screen",
+              "justify-between",
+              "flex-col",
+              "md:flex-row",
+            )}
+            initial={false}
+            animate={visible ? "visible" : "hidden"}>
+            <div
+              className={clsx(
+                "flex-1",
+                "flex",
+                "flex-col",
+                "items-center",
+                "justify-center",
+              )}>
+              <motion.div
+                className={clsx("p-8", "md:p-16")}
+                transition={{ staggerChildren: 0.07 }}>
+                <motion.h2
+                  className={clsx("text-2xl")}
+                  variants={{
+                    visible: {
+                      y: 0,
+                      opacity: 1,
+                      transition: {
+                        y: { stiffness: 1000, velocity: -100 },
+                      },
+                    },
+                    hidden: {
+                      y: 50,
+                      opacity: 0,
+                      transition: {
+                        y: { stiffness: 1000 },
+                      },
+                    },
+                  }}>
+                  Recog
+                </motion.h2>
+                <br />
+                <motion.p
+                  variants={{
+                    visible: {
+                      y: 0,
+                      opacity: 1,
+                      transition: {
+                        y: { stiffness: 1000, velocity: -100 },
+                      },
+                    },
+                    hidden: {
+                      y: 50,
+                      opacity: 0,
+                      transition: {
+                        y: { stiffness: 1000 },
+                      },
+                    },
+                  }}>
+                  Recog is a forum built in collaboration with the alte Kanti
+                  Aarau. It aims to connect students and business acquaintances
+                  across Switzerland with another and create a platform on which
+                  collaboration and a modern take on education is effortless.
+                </motion.p>
+              </motion.div>
+            </div>
+            <motion.div
+              className={clsx(
+                "flex-1",
+                "flex",
+                "justify-center",
+                "items-center",
+                "bg-gradient-to-br",
+                "from-blue-700",
+                "to-red-400",
+                "h-full",
+              )}
+              variants={{
+                visible: {
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    y: { stiffness: 1000, velocity: -100 },
+                  },
+                },
+                hidden: {
+                  y: 50,
+                  opacity: 0,
+                  transition: {
+                    y: { stiffness: 1000 },
+                  },
+                },
+              }}>
+              <Img
+                className={clsx("h-full", "w-3/4", "md:w-1/2", "m-6")}
+                imgStyle={{ objectFit: "contain" }}
+                fluid={recogLogo?.childImageSharp.fluid}
+              />
+            </motion.div>
+          </motion.article>
+        )}
+      </ViewableMonitor>
     </Layout>
   );
 };
